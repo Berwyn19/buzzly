7957d7ec9e236c2761bec0f1422e586b28197b7f
Berwyn <berwyn1915@gmail.com>
2025-07-09 13:41:30 +0700

    fixing firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -10,6 +10,7 @@ load_dotenv()
 # Only initialize if not already done (to prevent FastAPI reload issues)
 if not firebase_admin._apps:
     firebase_credentials = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
+    firebase_credentials["private_key"] = firebase_credentials["private_key"].replace("\\n", "\n")
     cred = credentials.Certificate(firebase_credentials)
     initialize_app(cred, {
         'storageBucket': 'ugcgenerator-5884a.appspot.com'  # Your actual bucket
